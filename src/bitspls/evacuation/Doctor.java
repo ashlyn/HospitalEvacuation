99b905371a14d13285cde1ffa578d59b48096531
Ashlyn Lee <ashlynlee13@gmail.com>
2016-04-06 19:38:50 -0500

    Model builder running - no logic in agents yet

---
+++ src/bitspls/evacuation/Doctor.java
@@ -0,0 +1,5 @@
+package bitspls.evacuation;
+
+public class Doctor {
+
+}
---
+++ src/bitspls/evacuation/HospitalEvacuationBuilder.java
@@ -0,0 +1,48 @@
+package bitspls.evacuation;
+
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.dataLoader.ContextBuilder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import repast.simphony.space.grid.WrapAroundBorders;
+
+public class HospitalEvacuationBuilder implements ContextBuilder<Object> {
+
+	@Override
+	public Context build(Context<Object> context) {
+		context.setId("HospitalEvacuation");
+
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder
+				.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace(
+				"space", context, new RandomCartesianAdder<Object>(),
+				new repast.simphony.space.continuous.WrapAroundBorders(), 50,
+				50);
+
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		Grid<Object> grid = gridFactory.createGrid("grid", context,
+				new GridBuilderParameters<Object>(new WrapAroundBorders(),
+						new SimpleGridAdder<Object>(), true, 50, 50));
+
+		int gasCount = 5;
+		for (int i = 0; i < gasCount; i++) {
+			context.add(new GasParticle(space, grid));
+		}
+
+		for (Object obj : context) {
+			NdPoint pt = space.getLocation(obj);
+			grid.moveTo(obj, (int) pt.getX(), (int) pt.getY());
+		}
+
+		return context;
+	}
+
+}
