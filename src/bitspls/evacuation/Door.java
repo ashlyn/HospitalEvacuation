0b15a7c30e3851e6c153b0f428afcf2d385869ce
Ashlyn Lee <ashlynlee13@gmail.com>
2016-04-13 21:34:19 -0500

    Patients can leave at the exits

--- src/bitspls/evacuation/Door.java
+++ src/bitspls/evacuation/Door.java
@@ -1,7 +1,21 @@
 package bitspls.evacuation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bitspls.evacuation.agents.GasParticle;
+import bitspls.evacuation.agents.Patient;
+import repast.simphony.context.Context;
+import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import repast.simphony.util.ContextUtils;
+import repast.simphony.util.SimUtilities;
 
 public class Door {
 	private ContinuousSpace<Object> space;
@@ -10,5 +24,33 @@ public class Door {
 	public Door(ContinuousSpace<Object> space, Grid<Object> grid) {
 		this.space = space;
 		this.grid = grid;
+	}
+	
+	@ScheduledMethod(start = 1, interval = 1)
+	public void allowPatientsToExit() {
+		GridPoint pt = this.grid.getLocation(this);
+		GridCellNgh<Patient> nghCreator = new GridCellNgh<Patient>(this.grid, pt, Patient.class, 1, 1);
+		List<GridCell<Patient>> gridCells = nghCreator.getNeighborhood(true);
+		SimUtilities.shuffle(gridCells, RandomHelper.getUniform());
+		
+		List<Patient> patients = new ArrayList<Patient>();
+		for (GridCell<Patient> cell : gridCells) {
+			for (Patient p : cell.items()) {
+				patients.add(p);
+			}
+		}
+		
+		Context<Object> context = ContextUtils.getContext(this);
+		if (patients.size() > 2) {
+			SimUtilities.shuffle(patients, RandomHelper.getUniform());
+			Patient p = patients.remove(0);
+			context.remove(p);
+			p = patients.remove(0);
+			context.remove(p);
+		} else if (patients.size() > 0) {
+			for (Patient p : patients) {
+				context.remove(p);
+			}
+		}
 	}
 }
