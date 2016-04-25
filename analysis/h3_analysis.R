808d800da8d01f7eb80c35479442f7a46b3861f6
Ashlyn Lee <ashlynlee13@gmail.com>
2016-04-24 21:26:28 -0500

    Added boxplots for survival rate and evacuation time

--- analysis/h3_analysis.R
+++ analysis/h3_analysis.R
@@ -12,4 +12,16 @@ efficiencyFromData <- function(data) {
 }
 
 data.20 <- read.table('../output/end_count.2016.Apr.24.19_15_59.txt', header=TRUE, sep=',')
+data.other <- read.table('../output/end_count.2016.Apr.24.17_11_43.txt', header=TRUE, sep=',')
 efficiency.20 <- efficiencyFromData(data.20)
+efficiency.other <- efficiencyFromData(data.other)
+
+survival <- c(efficiency.20["efficiency.t"], efficiency.other["efficiency.t"])
+names(survival) <- c("1:20", "Other")
+boxplot(survival)
+title(main="Survival Rate", ylab="Survial Rate")
+
+time <- c(efficiency.20["time"], efficiency.other["time"])
+names(time) <- c("1:20", "Other")
+boxplot(time)
+title(main="Evacuation Time", ylab="Ticks")
